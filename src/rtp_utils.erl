1af6df4eaf1747ea84a86061aa65f9819b22d1d3
Peter Lemenkov <lemenkov@gmail.com>
2011-03-01 18:32:21 +0300

    Added rtp_utils:get_type/1 function
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtp_utils.erl
+++ src/rtp_utils.erl
@@ -31,9 +31,13 @@
 -author('lemenkov@gmail.com').
 
 -export([dump_packet/3]).
+-export([get_type/1]).
+
 -export([ntp2now/2]).
 -export([now2ntp/0]).
 -export([now2ntp/1]).
+
+-include("../include/rtcp.hrl").
 
 dump_packet(Node, Pid, Packet) ->
 	{H,M,Ms} = now(),
@@ -41,22 +45,15 @@ dump_packet(Node, Pid, Packet) ->
 	% lists:map(fun(X) -> io:format("FILE: ~p~n", [X]), {ok, Rtcp} = file:read_file(X), rtcp:decode(Rtcp) end, filelib:wildcard("/tmp/rtcp_err.*.bin")).
 	file:write_file("/tmp/rtcp_err." ++ atom_to_list(Node) ++ "." ++ pid_to_list(Pid) ++ "." ++ integer_to_list(H) ++ "_" ++ integer_to_list(M) ++ "_" ++ integer_to_list(Ms) ++ ".bin", Packet).
 
-ntp2now (NTPSec, NTPFrac) ->
-	MegaSecs = (NTPSec - 2208988800) div 1000000,
-	Secs = (NTPSec - 2208988800) rem 1000000,
-	R = lists:foldl(fun(X, Acc) -> Acc + ((NTPFrac bsr (X-1)) band 1)/(2 bsl (32-X)) end, 0, lists:seq(1, 32)),
-	MicroSecs = trunc(1000000*R),
-	{MegaSecs, Secs, MicroSecs}.
-
-
-now2ntp () ->
-	now2ntp (now()).
-
-now2ntp ({MegaSecs, Secs, MicroSecs}) ->
-	% 2208988800 is the number of seconds from 00:00:00 01-01-1900 to 00:00:00 01-01-1970
-	NTPSec = MegaSecs*1000000 + Secs + 2208988800,
-	{NTPSec, frac(MicroSecs)}.
-
+get_type(#fir{}) -> fir;
+get_type(#nack{}) -> nack;
+get_type(#sr{}) -> sr;
+get_type(#rr{}) -> rr;
+get_type(#sdes{}) -> sdes;
+get_type(#bye{}) -> bye;
+get_type(#app{}) -> app;
+get_type(#xr{}) -> xr;
+get_type(_) -> unknown.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%
@@ -73,3 +70,19 @@ frac(Int, X, Acc) ->
 	Div = Int div (2 bsl X-1),
 	Rem = Int rem (2 bsl X-1),
 	frac(Rem, X-1, Acc bor (Div bsl X)).
+
+ntp2now (NTPSec, NTPFrac) ->
+	MegaSecs = (NTPSec - 2208988800) div 1000000,
+	Secs = (NTPSec - 2208988800) rem 1000000,
+	R = lists:foldl(fun(X, Acc) -> Acc + ((NTPFrac bsr (X-1)) band 1)/(2 bsl (32-X)) end, 0, lists:seq(1, 32)),
+	MicroSecs = trunc(1000000*R),
+	{MegaSecs, Secs, MicroSecs}.
+
+
+now2ntp () ->
+	now2ntp (now()).
+
+now2ntp ({MegaSecs, Secs, MicroSecs}) ->
+	% 2208988800 is the number of seconds from 00:00:00 01-01-1900 to 00:00:00 01-01-1970
+	NTPSec = MegaSecs*1000000 + Secs + 2208988800,
+	{NTPSec, frac(MicroSecs)}.
