f734d7e00947a03b831f738328b033f5f0049013
Peter Lemenkov <lemenkov@gmail.com>
2012-02-18 10:15:43 +0400

    Allow all speex tests to pass
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- test/codec_speex_test.erl
+++ test/codec_speex_test.erl
@@ -8,13 +8,21 @@ codec_speex_test_() ->
 		{"Test decoding from SPEEX to PCM",
 			fun() -> ?assertEqual(
 						true,
-						test_utils:codec_decode(
-							"../test/samples/speex/sample-speex-16-mono-8khz.raw",
-							"../test/samples/speex/sample-pcm-16-mono-8khz.from_spx",
-							38,
-							"SPEEX",
-							{'SPEEX',8000,1}
-						)
+						(fun() ->
+							{ok, BinIn}  = file:read_file("../test/samples/speex/sample-speex-16-mono-8khz.raw"),
+							{ok, PcmOut} = file:read_file("../test/samples/speex/sample-pcm-16-mono-8khz.from_spx"),
+							{ok, Codec} = codec:start_link({'SPEEX',8000,1}),
+							Ret = test_utils:decode_f("SPEEX", Codec, BinIn, PcmOut, 38),
+							codec:close(Codec),
+							Ret
+						end)()
+%						test_utils:codec_decode(
+%							"../test/samples/speex/sample-speex-16-mono-8khz.raw",
+%							"../test/samples/speex/sample-pcm-16-mono-8khz.from_spx",
+%							38,
+%							"SPEEX",
+%							{'SPEEX',8000,1}
+%						)
 					) end
 		},
 		{"Test encoding from PCM to SPEEX",
