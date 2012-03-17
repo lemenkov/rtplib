9c3a265ea19fae111fd9b46763a9c36a9222bc75
Peter Lemenkov <lemenkov@gmail.com>
2012-03-17 23:15:32 +0400

    Add licensing terms (closes #28)
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtp.erl
+++ src/rtp.erl
@@ -1,5 +1,5 @@
 %%%----------------------------------------------------------------------
-%%% Copyright (c) 2008-2010 Peter Lemenkov <lemenkov@gmail.com>
+%%% Copyright (c) 2008-2012 Peter Lemenkov <lemenkov@gmail.com>
 %%%
 %%% All rights reserved.
 %%%
@@ -11,8 +11,9 @@
 %%% * Redistributions in binary form must reproduce the above copyright notice,
 %%% this list of conditions and the following disclaimer in the documentation
 %%% and/or other materials provided with the distribution.
-%%% * Neither the names of its contributors may be used to endorse or promote
-%%% products derived from this software without specific prior written permission.
+%%% * Neither the name of the authors nor the names of its contributors
+%%% may be used to endorse or promote products derived from this software
+%%% without specific prior written permission.
 %%%
 %%% THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ''AS IS'' AND ANY
 %%% EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
--- test/vendor_unknown_test.erl
+++ test/vendor_unknown_test.erl
@@ -1,3 +1,33 @@
+%%%----------------------------------------------------------------------
+%%% Copyright (c) 2008-2012 Peter Lemenkov <lemenkov@gmail.com>
+%%%
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without modification,
+%%% are permitted provided that the following conditions are met:
+%%%
+%%% * Redistributions of source code must retain the above copyright notice, this
+%%% list of conditions and the following disclaimer.
+%%% * Redistributions in binary form must reproduce the above copyright notice,
+%%% this list of conditions and the following disclaimer in the documentation
+%%% and/or other materials provided with the distribution.
+%%% * Neither the name of the authors nor the names of its contributors
+%%% may be used to endorse or promote products derived from this software
+%%% without specific prior written permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ''AS IS'' AND ANY
+%%% EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+%%% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+%%% DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+%%% DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+%%% ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+%%% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%
+%%%----------------------------------------------------------------------
+
 -module(vendor_unknown_test).
 
 -include("rtcp.hrl").
@@ -29,7 +59,7 @@ vendor_unknown_test_() ->
 	Sr2 = #sr{ssrc=928159846, ntp=15120798205620938342, timestamp=25259240, packets=151840, octets=26116480, rblocks=[]},
 	Sdes2 = #sdes{list=[[{ssrc,928159846},{cname,"A424CC73125A420DBD5BCF1AABED9CCF@unique.z23EA517BD3034B5B.org"},{priv,{"x-rtp-session-id",<<"30F2E3713D4E469BA32BEC4A05E629DB">>}},{eof,true}]]},
 	Bye2 = #bye{message=[], ssrc=[928159846]},
-	
+
 	[
 		{"Encode the entire RR+SDES+BYE packet",
 			fun() -> ?assertEqual({ok, [Rr1, Sdes1, Bye1]}, rtcp:decode(RrSdesBye1Bin)) end
