5bd4be96321045db2f71bede77570a238cda3785
Peter Lemenkov <lemenkov@gmail.com>
2012-03-29 14:09:25 +0400

    Another attempt to unify endianness handling
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- c_src/endianness.c
+++ c_src/endianness.c
@@ -4,38 +4,12 @@
  *       It is public domain.
  * */
 
-#ifdef __APPLE__
-
-#include <libkern/OSByteOrder.h>
-
-#define htobe16(x) OSSwapHostToBigInt16(x)
-#define htole16(x) OSSwapHostToLittleInt16(x)
-#define be16toh(x) OSSwapBigToHostInt16(x)
-#define le16toh(x) OSSwapLittleToHostInt16(x)
-
-#define htobe32(x) OSSwapHostToBigInt32(x)
-#define htole32(x) OSSwapHostToLittleInt32(x)
-#define be32toh(x) OSSwapBigToHostInt32(x)
-#define le32toh(x) OSSwapLittleToHostInt32(x)
-
-#define htobe64(x) OSSwapHostToBigInt64(x)
-#define htole64(x) OSSwapHostToLittleInt64(x)
-#define be64toh(x) OSSwapBigToHostInt64(x)
-#define le64toh(x) OSSwapLittleToHostInt64(x)
-
-#else
-
-#include <endian.h>
-
-#endif
-
-#include <stdint.h>
-#include <stddef.h>
+#include <arpa/inet.h>
 
 void htobe16_map(int16_t* buffer, size_t size)
 {
 	size_t i = 0;
 	for(i = 0; i < size; i++)
-		buffer[i] = htobe16(buffer[i]);
+		buffer[i] = ntohs(buffer[i]);
 	return;
 }
