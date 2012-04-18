ef930de6f894c7610d56ec5e109c396a765670c4
Peter Lemenkov <lemenkov@gmail.com>
2012-04-18 19:14:18 +0400

    Pad with spaces
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/stun.erl
+++ src/stun.erl
@@ -189,9 +189,10 @@ encode_bin({T, V}) ->
 	L = size(V),
 	PaddingLength = case L rem 4 of
 		0 -> 0;
-		Else -> (4 - Else)*8
+		Else -> 4 - Else
 	end,
-	<<T:16, L:16, V:L/binary, 0:PaddingLength>>.
+	Padding = << <<16#20:8>> || X <- lists:seq(0,PaddingLength-1) >>,
+	<<T:16, L:16, V:L/binary, Padding:PaddingLength/binary>>.
 
 encode_attr('MAPPED-ADDRESS', Value, _) -> {16#0001, encode_attr_addr(Value)};
 encode_attr('RESPONSE-ADDRESS', Value, _) -> {16#0002, encode_attr_addr(Value)};
