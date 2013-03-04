2cfc6999baff0649cdd569629ecebec002af1c57
Peter Lemenkov <lemenkov@gmail.com>
2013-03-04 17:02:36 +0400

    Remove leftover
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- test/vendor_mera_test.erl
+++ test/vendor_mera_test.erl
@@ -63,7 +63,6 @@ vendor_mera_test_() ->
 
 	[
 		{"Check that we won't fail on parsing broken RTCP",
-%			fun() -> ?assertEqual({ok, #rtcp{payloads = [Sr], encrypted = RtcpBroken}},rtcp:decode(RtcpBroken)) end
 			fun() -> ?assertEqual({ok, #rtcp{payloads = [Sr, Sdes]}},rtcp:decode(RtcpBroken)) end
 		},
 		{"Check that we can parse fixed RTCP",
